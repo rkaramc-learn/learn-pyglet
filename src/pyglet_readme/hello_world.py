2cf5af95bb15800b387ca35140661aa1bcd51358
Rajeev Karamcheti <rkaramc@gmail.com>
2026-01-04 22:03:19 +0530

    added basedpyright to dev dependencies

--- src/pyglet_readme/hello_world.py
+++ src/pyglet_readme/hello_world.py
@@ -3,10 +3,12 @@ import pyglet
 window = pyglet.window.Window()
 
 label = pyglet.text.Label('Hello, world!', font_size=36, x=window.width // 2, y = window.height // 2, anchor_x = 'center', anchor_y = 'center')
+image = pyglet.resource.image('kitten.jpg')
 
-@window.event
+@window.event  # pyright: ignore[reportUnknownMemberType]
 def on_draw():
     window.clear()
     label.draw()
+    image.blit(0, 0)
 
 pyglet.app.run()
