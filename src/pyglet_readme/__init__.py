a79cbc91260636d60abd7bfca1582863694f3df3
Rajeev Karamcheti <rkaramc@gmail.com>
2026-01-04 22:09:06 +0530

    refactor(hello_world): encapsulate logic and call from main
    - Refactor hello_world.py to wrap logic in un_hello_world function.
    - safeguard against missing kitten.jpg asset.
    - Update __init__.py to call un_hello_world.

--- src/pyglet_readme/__init__.py
+++ src/pyglet_readme/__init__.py
@@ -1,2 +1,5 @@
+from .hello_world import run_hello_world
+
 def main() -> None:
     print("Hello from pyglet-readme!")
+    run_hello_world()
--- src/pyglet_readme/hello_world.py
+++ src/pyglet_readme/hello_world.py
@@ -1,14 +1,18 @@
 import pyglet
 
-window = pyglet.window.Window()
+def run_hello_world():
+    window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello, world!', font_size=36, x=window.width // 2, y = window.height // 2, anchor_x = 'center', anchor_y = 'center')
-image = pyglet.resource.image('kitten.jpg')
+    label = pyglet.text.Label('Hello, world!', font_size=36, x=window.width // 2, y = window.height // 2, anchor_x = 'center', anchor_y = 'center')
+    # image = pyglet.resource.image('kitten.jpg')
 
-@window.event  # pyright: ignore[reportUnknownMemberType]
-def on_draw():
-    window.clear()
-    label.draw()
-    image.blit(0, 0)
+    @window.event  # pyright: ignore[reportUnknownMemberType]
+    def on_draw(): # pyright: ignore[reportUnusedFunction]
+        window.clear()
+        label.draw()
+        # image.blit(0, 0)
 
-pyglet.app.run()
+    pyglet.app.run()
+
+if __name__ == "__main__":
+    run_hello_world()
